c023b06d8b8dc0989421b4b9ce093362458bacaf
Hannah Zeng <tz2486@barnard.edu>
2022-02-09 16:35:10 -0500

    everything except dynamic bouncing off for each letter

--- sketch_ecology/sketch_ecology.ino
+++ sketch_ecology/sketch_ecology.ino
@@ -4,12 +4,10 @@
 
 TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
-char vow[5] = {'a', 'e', 'i', 'o', 'u'};
-char con[22] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
-  'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
-char con2[13] = {'b', 'd', 'g', 'k', 'l', 'm', 'n', 'p', 'r', 't', 'w', 'y'};
+//fixed
+char wordList[10] = "create";
 
-int xspeed = 10;
+int xspeed = 5;
 int yspeed = 2;
 
 void setup(void) {
@@ -22,6 +20,8 @@ void setup(void) {
 void loop() {
   tft.fillScreen(TFT_BLACK);
   tft.setTextSize(2.9);
+  tft.setTextColor(random(0xFFFF));
+
 
   // Draw some randomly generated C-V words
   int i = random(0,6);
@@ -30,54 +30,58 @@ void loop() {
   int ry = random(40);
   int x = rx + random(160 - rx - rx);
   int y = 24*i;
-  tft.setTextColor(random(0xFFFF));
-
-  //fixed head and tail
-  String head = String(con[ random(0,21) ]);
-    head.toUpperCase();
-  String tail = String(con2[ random(0,12) ]);
-  String body = String(vow[ random(0,5) ]);
-
   
   for (int h = y; h < 120; h+=8)
   {
 
     //falling
     y = h;
-    tft.drawString(head+body+tail, x, y, 2);
+    tft.drawString(
+      String(wordList[0])+
+      String(wordList[1])+
+      String(wordList[2])+
+      String(wordList[3])+
+      String(wordList[4])+
+      String(wordList[5]), x, y, 2);
     delay(60);
     tft.fillScreen(TFT_BLACK);
 
     
-    }
+  }
 
     //if hit the bottom, bounce off
-    if (y = 110) {
-      int x1 = x - 30;
-      int x2 = x;
-      int x3 = x + 30;
+    if (y = 90) {
       
       tft.fillScreen(TFT_BLACK);
-      for (; y > 0; y -= 1){
+      int count = 0;
+      while (count < 10){
         
-        if ((x == 20 || x == 100) && (x == 160 || x == 20)){
+        if ((x < 20 || x > 100) || (y > 150 || y < 5)){
           xspeed *=(-1);
           yspeed *=(-1);
+
+          count++;
         }
-          
+
         y -= yspeed;
-        x1 += xspeed*0.5;
-        x2 += xspeed*0.7;
-        x3 += xspeed*(-0.5);
-        tft.drawString(head, x1, y, 2);
-        tft.drawString(body, x2, y, 2);
-        tft.drawString(tail, x3, y, 2);
-        delay(20);
+        x += xspeed;
+
+        //give letters diff speed
+        tft.drawString(String(wordList[0]), x-20, y, 2);
+        tft.drawString(String(wordList[1]), x-10, y, 2);
+        tft.drawString(String(wordList[2]), x-5, y, 2);
+        tft.drawString(String(wordList[3]), x, y, 2);
+        tft.drawString(String(wordList[4]), x+5, y, 2);
+        tft.drawString(String(wordList[5]), x+10, y, 2);
+
+        delay(60);
 
         tft.fillScreen(TFT_BLACK);
+        tft.drawString(String("Ecology is balance."), 0, 0);
 
         
       }
+      
     
   }
 
